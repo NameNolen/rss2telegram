00bfee905ec878a08c2144e57fa19cadaada03f0
NameNolen <renpeng18@live.com>
2019-03-02 21:47:34 +0800

    翻译注释

--- rss.py
+++ rss.py
@@ -23,8 +23,8 @@ Base = declarative_base()
 
 
 class Source(object):
-	""" Класс для парсинга RSS-канала.
-	Выделяет из общей информации только интереующие нас поля: Заголовок, ссылку, дату публикации.
+	""" 用于解析RSS提要的类。
+            从一般信息中仅选择我们感兴趣的字段：标题，链接，发布日期。
 	"""
 	def __init__(self, config_links):
 		self.links = [config_links[i] for i in config_links]
@@ -56,21 +56,20 @@ class Bitly:
 
 class News(Base):	
 	"""
-	Класс, описывающий объект новости. Так же, осуществляется взаимодействие с БД.
-	Описание полей таблицы ниже.
+	描述新闻项的类。 此外，与数据库的交互。下表中字段的描述。
 	"""
 	__tablename__ = 'news'
-	id = Column(Integer, primary_key=True) # Порядковый номер новости
-	text = Column(String) # Текст (Заголовок), который будет отправлен в сообщении
-	link  = Column(String) # Ссылка на статью на сайте. Так же отправляется в сообщении
+	id = Column(Integer, primary_key=True) # 订单号新闻
+	text = Column(String) # 要在消息中发送的文本（标题）
+	link  = Column(String) # 链接到网站上的文章。 还发送了一条消息
 	date = Column(Integer)
-	# Дата появления новости на сайте. Носит Чисто информационный характер. UNIX_TIME.
+	# 网站上的新闻发布日期。 这纯粹是信息性的。UNIX_TIME。
 	publish = Column(Integer)
-	# Планируемая дата публикации. Сообщение будет отправлено НЕ РАНЬШЕ этой даты. UNIX_TIME.
+	# 计划发布日期。 该消息将在此日期之前发送。UNIX_TIME。
 	chat_id = Column(Integer) 
-	# Информационный столбец. В данной версии функциональной нагрузки не несет.
+	# 信息栏。 在此版本中，功能负载不携带。
 	message_id = Column(Integer) 
-	# Информационный столбец. В данной версии функциональной нагрузки не несет.
+	# 信息栏。 在此版本中，功能负载不携带。
 
 	def __init__(self, text, link, date, publish=0,chat_id=0,message_id=0):
 		self.link = link
@@ -93,13 +92,11 @@ class News(Base):
 		return "<News ('%s','%s', %s)>" % (base64.b64decode(self.text).decode(),\
 			base64.b64decode(self.link).decode(),\
 			datetime.fromtimestamp(self.publish))
-			# Для зрительного восприятия данные декодируется
+			＃对于视觉感知数据被解码
 
 class Database:
 	"""
-	Класс для обработки сессии SQLAlchemy.
-	Так же включает в себя минимальный набор методов, вызываемых в управляющем классе.
-	Названия методов говорящие.
+	用于处理SQLAlchemy会话的类。它还包括控件类中调用的最小方法集。方法的名称说。
 	"""
 	def __init__(self, obj):
 		engine = create_engine(obj, echo=False)
@@ -138,12 +135,12 @@ class ExportBot:
 		self.bit_ly = Bitly(config['Bitly']['access_token'])
 	
 	def detect(self):
-		#получаем 30 последних постов из rss-канала
+		＃我们从rss-channel获得最后30个帖子
 		self.src.refresh()
 		news = self.src.news		
 		news.reverse()
-		#Проверяем на наличие в базе ссылки на новость, если нет, то добавляем в базу данных с 
-		#отложенной публикацией
+		＃检查数据库中是否存在新闻链接，如果没有，则添加到数据库中
+                ＃推迟出版
 		for i in news:
 			if not self.db.find_link(i.link):
 				now = int(time.mktime(time.localtime()))
@@ -152,14 +149,14 @@ class ExportBot:
 				self.db.add_news(i)
 
 	def public_posts(self):
-		#Получаем 30 последних записей из rss канала и новости из БД, у которых message_id=0
+		＃我们从rss频道获得30个最近的条目，从数据库获得新闻，其中message_id = 0
 		posts_from_db = self.db.get_post_without_message_id()
 		self.src.refresh()
 		line = [i for i in self.src.news]
-		#Выбор пересечний этих списков
+		＃选择这些列表的交叉线
 		for_publishing = list(set(line) & set(posts_from_db))
 		for_publishing = sorted(for_publishing, key=lambda news: news.date)
-		#Постинг каждого сообщений
+		＃发布每条消息
 		for post in for_publishing:
 			text = '%s %s' % (base64.b64decode(post.text).decode('utf8'),\
 							  self.bit_ly.short_link(base64.b64decode(post.link).decode('utf-8')))
